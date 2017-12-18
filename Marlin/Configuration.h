cb935766bbf87c2309d367a76750d8d950075b9f
Simon Paarlberg <simon@paarlberg.dk>
2017-12-18 08:25:19 +0100

    Changes bedsize

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -816,7 +816,7 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 180
+#define X_BED_SIZE 165
 #define Y_BED_SIZE 180
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
